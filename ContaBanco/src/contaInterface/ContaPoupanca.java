1401babb6cb2c4b059886441baf4a21906ec3434
Diego Ferreira <diego19912011@live.com>
2025-04-29 22:33:02 -0300

    Preparação para projeto Banco

---
+++ ContaBanco/src/contaInterface/ContaPoupanca.java
@@ -0,0 +1,16 @@
+package contaInterface;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+        
+    }
+
+    @Override
+    public void imprimirExtrado(){
+        System.out.println("========= Extrato Conta Poupança =========");
+        super.imprimirInfosComuns();
+    }
+    
+}
